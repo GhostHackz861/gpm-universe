19ef9f8363313bc7fedea03293068af2312cae0e
GhostHackz <nepaligaming@icloud.com>
2017-12-02 03:56:09 +0000

    Commit

---
+++ meancalc/app.py
@@ -0,0 +1,11 @@
+import sys
+
+def main(numbers):
+	total = 0
+	for number in numbers:
+		total += int(number)
+	mean = total / len(numbers)
+	print(mean)
+	
+if __name__ == "__main__":
+	main(sys.argv[1:])
